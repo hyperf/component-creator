9479f882c61b4fa5799a7abf8bf781621da46022
李铭昕 <715557344@qq.com>
2022-08-08 21:33:26 +0800

    Upgrade the minimum php version to 8.0

--- installer/config.php
+++ installer/config.php
@@ -12,10 +12,10 @@ declare(strict_types=1);
 return [
     'packages' => [
         'hyperf/framework' => [
-            'version' => '2.2.*',
+            'version' => '3.0.*',
         ],
         'hyperf/di' => [
-            'version' => '2.2.*',
+            'version' => '3.0.*',
         ],
     ],
     'require-dev' => [
